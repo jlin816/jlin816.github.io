bc7ec5dda22361bc217a58d5c2c8ad9431013420
jlin816 <jessy81697@gmail.com>
2019-04-15 13:18:37 -0400

    Compressed photos with PIL, way faster

--- _site/images/photography/processPhotos.py
+++ _site/images/photography/processPhotos.py
@@ -1,14 +1,24 @@
 from PIL import Image
 import os
+import math
+
+MAX_WIDTH = 600
 
 # Add aspect ratio to img name:
 # IMG_0786--.667-
-for filename in os.listdir("thumbnails"):
-    if filename.endswith(".jpg") and "--" not in filename:
-        im = Image.open("thumbnails/" + filename)
-        aspect_ratio = im.size[1] / im.size[0]
+for filename in os.listdir("original"):
+    if filename.endswith(".jpg"):
+        im = Image.open("original/" + filename)
+        w, h = im.size
+        print(im.size)
+        aspect_ratio = h / w
         new_filename = "{}--{:.3f}--.jpg".format(os.path.splitext(filename)[0], aspect_ratio)
         print(filename)
         print("Aspect ratio: ", aspect_ratio)
-        print(new_filename)
-        os.rename("thumbnails/" + filename, "thumbnails/" + new_filename)
+        print("Saving to ", new_filename)
+
+        # Resize to max width 
+        downsize = MAX_WIDTH / w
+        print("Downsizing to ({},{})".format(downsize * im.size[0], downsize * im.size[1]))
+        im = im.resize((math.ceil(downsize * im.size[0]), math.ceil(downsize * im.size[1])), Image.LANCZOS)
+        im.save("thumbnails/" + new_filename, optimize=True, quality=85)
